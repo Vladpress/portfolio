df7377e5b948ed919ab33ba7fded3ed8cd7a1191
Vladpress <pixel22yuliya@gmail.com>
2021-04-09 17:39:00 +0300

    Updates

---
+++ precache-manifest.215ae92206de2624180b518895f87398.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3874d193c6caf1cf8fda7c6321e706e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c17a7035d77be784aba2",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "609d5cf8994bad59f0cf",
+    "url": "/portfolio/static/css/main.389c3390.chunk.css"
+  },
+  {
+    "revision": "c17a7035d77be784aba2",
+    "url": "/portfolio/static/js/2.3bcbdedb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3bcbdedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609d5cf8994bad59f0cf",
+    "url": "/portfolio/static/js/main.3327da18.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
